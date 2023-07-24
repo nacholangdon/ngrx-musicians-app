ef8137fa274bdf81d715e0b0887faf3cd49235f8
Nacho Langdon <nacholangdon@gmail.com>
2023-07-24 14:17:39 -0300

    Remove initial slash from photoUrl (musicians)

--- src/app/musicians/musicians.service.ts
+++ src/app/musicians/musicians.service.ts
@@ -15,31 +15,31 @@ const musiciansMock: Musician[] = [
   {
     id: 1,
     name: 'Eric Clapton',
-    photoUrl: '/assets/musicians/eric-clapton.jpg',
+    photoUrl: 'assets/musicians/eric-clapton.jpg',
   },
   {
     id: 2,
     name: 'Stevie Ray Vaughan',
-    photoUrl: '/assets/musicians/srv.jpg',
+    photoUrl: 'assets/musicians/srv.jpg',
   },
   {
     id: 3,
     name: 'B.B. King',
-    photoUrl: '/assets/musicians/bb-king.jpg',
+    photoUrl: 'assets/musicians/bb-king.jpg',
   },
   {
     id: 4,
     name: 'Gary Moore',
-    photoUrl: '/assets/musicians/gary-moore.jpg',
+    photoUrl: 'assets/musicians/gary-moore.jpg',
   },
   {
     id: 5,
     name: 'Jimi Hendrix',
-    photoUrl: '/assets/musicians/jimi-hendrix.jpg',
+    photoUrl: 'assets/musicians/jimi-hendrix.jpg',
   },
   {
     id: 6,
     name: 'Keith Richards',
-    photoUrl: '/assets/musicians/keith-richards.jpg',
+    photoUrl: 'assets/musicians/keith-richards.jpg',
   },
 ];
