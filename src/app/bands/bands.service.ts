cfe8b40ca8fb835f328c3f50f1773e04199d5529
Nacho Langdon <nacholangdon@gmail.com>
2023-07-24 14:13:32 -0300

    Remove slash from photoUrl

--- src/app/bands/bands.service.ts
+++ src/app/bands/bands.service.ts
@@ -15,31 +15,31 @@ const bandsMock: Band[] = [
   {
     id: 1,
     name: 'The Beatles',
-    photoUrl: '/assets/bands/the-beatles.jpg',
+    photoUrl: 'assets/bands/the-beatles.jpg',
   },
   {
     id: 2,
     name: 'R.E.M',
-    photoUrl: '/assets/bands/rem.jpg',
+    photoUrl: 'assets/bands/rem.jpg',
   },
   {
     id: 3,
     name: 'The Rolling Stones',
-    photoUrl: '/assets/bands/the-rolling-stones.jpg',
+    photoUrl: 'assets/bands/the-rolling-stones.jpg',
   },
   {
     id: 4,
     name: 'The Who',
-    photoUrl: '/assets/bands/the-who.jpg',
+    photoUrl: 'assets/bands/the-who.jpg',
   },
   {
     id: 5,
     name: 'U2',
-    photoUrl: '/assets/bands/u2.jpg',
+    photoUrl: 'assets/bands/u2.jpg',
   },
   {
     id: 6,
     name: 'Queen',
-    photoUrl: '/assets/bands/queen.jpg',
+    photoUrl: 'assets/bands/queen.jpg',
   },
 ];
